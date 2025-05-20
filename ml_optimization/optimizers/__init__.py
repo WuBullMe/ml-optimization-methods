64d1a1a893fd3127436d569c5926b73eda6b2f5b
Muhammadjon Hokimiyon <hokimiyon04@gmail.com>
2025-05-20 20:07:32 +0300

    first phase

--- ml_optimization/optimizers/__init__.py
+++ ml_optimization/optimizers/__init__.py
@@ -1,3 +1,4 @@
 from .kd.kd_optimization import KDOptimization
 from .lora.lora_optimization import LoROptimization
 from .prune.prune_optimization import PruneOptimization
+from .quantization.quantization_optimization import QuantizationOptimization
